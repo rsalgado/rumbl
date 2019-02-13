c26bfbe18bf3297b660fe5329c67d5214817195e
Roberto Salgado <rsalgado@vsiteam.com>
2019-02-13 17:39:05 -0500

    Add socket authentication. Refactor the Auth plug to include token in the assigns. Put that token in the layout, read that token from JS, and verify that token on the server-side when connecting to UserSocket

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -15,9 +15,22 @@ defmodule RumblWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+      socket,
+      "user socket",
+      token,
+      max_age: @max_age
+    ) do
+      {:ok, user_id} -> {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} -> :error
+    end
   end
+
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -29,5 +42,5 @@ defmodule RumblWeb.UserSocket do
   #     RumblWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -12,11 +12,11 @@ defmodule RumblWeb.Auth do
     cond do
       # This was added to make testing easier. We leave the connection intact;
       # No matter how the `:current_user` got in the assigns, we honor its presence
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
       # This is the normal behavior (there's a valid user_id), so we set the `:current_user` assign
       user = user_id && Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       # Otherwise (no user_id or invalid user_id)
       _otherwise = true ->
@@ -50,12 +50,21 @@ defmodule RumblWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  # Add into the assigns the entries for `:current_user` and `:user_token`
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
