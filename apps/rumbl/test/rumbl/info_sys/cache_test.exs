484a4545c4768069d29096a9158080a2a1219919
Roberto Salgado <rsalgado@vsiteam.com>
2019-03-22 19:26:33 -0500

    Add tests for InfoSys.Cache's corner cases

--- apps/rumbl/test/rumbl/info_sys/cache_test.exs
+++ apps/rumbl/test/rumbl/info_sys/cache_test.exs
@@ -4,6 +4,24 @@ defmodule Rumbl.InfoSys.CacheTest do
 
   @moduletag clear_interval: 100
 
+  defp assert_shutdown(pid) do
+    ref = Process.monitor(pid)
+    Process.unlink(pid)
+    Process.exit(pid, :kill)
+
+    assert_receive {:DOWN, ^ref, :process, ^pid, :killed}
+  end
+
+  defp eventually(func) do
+    if func.() do
+      true
+    else
+      Process.sleep(10)
+      eventually(func)
+    end
+  end
+
+
   setup %{test: name, clear_interval: clear_interval} do
     {:ok, pid} = Cache.start_link(name: name, clear_interval: clear_interval)
     {:ok, name: name, pid: pid}
@@ -20,5 +38,19 @@ defmodule Rumbl.InfoSys.CacheTest do
 
   test "unfound entry returns error", %{name: name} do
     assert Cache.fetch(name, :notexists) == :error
+  end
+
+  test "clears all entries after clear interval", %{name: name} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert Cache.fetch(name, :key1) == {:ok, :value1}
+    assert eventually(fn -> Cache.fetch(name, :key1) == :error end)
+  end
+
+  @tag clear_interval: 60_000
+  test "values are cleaned up on exit", %{name: name, pid: pid} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert_shutdown(pid)
+    {:ok, _cache} = Cache.start_link(name: name)
+    assert Cache.fetch(name, :key1) == :error
   end
 end
